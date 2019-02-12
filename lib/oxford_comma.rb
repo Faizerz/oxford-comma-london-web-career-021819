f89d00dec713b3e5b603c58b172ee5c7ed60799e
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-12 18:09:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
-    return array
+    return array[0]
    elsif array.length == 2
-    return array.split(" and ")
+    return array.join(" and ")
   else
   end
 end
